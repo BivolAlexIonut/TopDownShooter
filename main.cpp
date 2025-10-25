5abfdf6387669baa166aca4f8597e3fe1add9081
Alex <bivol.allex@gmail.com>
2025-10-25 23:28:34 +0300

    Am confiigurat cmake

--- main.cpp
+++ main.cpp
@@ -1,119 +1,117 @@
-#include <iostream>
-#include <array>
-#include <chrono>
-#include <thread>
-
 #include <SFML/Graphics.hpp>
-
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
-public:
-    explicit SomeClass(int) {}
-};
-
-SomeClass *getC() {
-    return new SomeClass{2};
-}
-//////////////////////////////////////////////////////////////////////
+#include "Player.h"
+#include "GameMap.h"
+#include "Bullet.h"
+#include <iostream>
+#include <vector>
+#include "Enemy.h"
+#include <ostream>
 
 
 int main() {
-    ///
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+    sf::RenderWindow window(sf::VideoMode({1280, 720}), "Top-Down Shooter");
+    window.setFramerateLimit(60);
+
+    GameMap gameMap;
+    if (!gameMap.load("../assets/Levels/level1.json",
+                      "../assets/Premium Content/Tileset with cell size 256x256.png")) {
+        std::cerr << "EROARE FATALA: Harta nu a putut fi incarcata." << std::endl;
+        return -1;
+                      }
+    constexpr float mapScale = 0.4f;
+    Player player(1000.f * mapScale, 1000.f * mapScale);
+
+    Enemy enemy1(100.f, 200.f);
+    std::vector<Enemy> enemies;
+    enemies.push_back(enemy1);
+
+    player.takeDamage(25.f);
+    enemies[0].takeDamage(10.f);
+
+    sf::Clock clock;
+    sf::View camera;
+    camera.setSize({1280,720});
+    camera.setCenter(player.getPosition());
+
+    //Bullets
+    std::vector<Bullet> bullets;
+    sf::Clock shootTimer;
+
+    //UI Ammo
+    sf::Font ammoFont;
+    if (!ammoFont.openFromFile("../fonts/m6x11.ttf")) {
+        std::cerr << "EROARE: Nu am putut incarca fontul assets/arial.ttf" << std::endl;
+        return -1;
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
+    sf::Text ammoText(ammoFont);
+    ammoText.setFont(ammoFont);
+    ammoText.setCharacterSize(56);
+    ammoText.setFillColor(sf::Color::Black);
 
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;  // comentarea acestui rând ar trebui să ducă la semnalarea unui mem leak
-
-    sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    std::cout << "Fereastra a fost creată\n";
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                                    ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
-
-    while(window.isOpen()) {
-        bool shouldExit = false;
-
-        while(const std::optional event = window.pollEvent()) {
+        while (auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
-                std::cout << "Fereastra a fost închisă\n";
             }
-            else if (event->is<sf::Event::Resized>()) {
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-            }
-            else if (event->is<sf::Event::KeyPressed>()) {
-                const auto* keyPressed = event->getIf<sf::Event::KeyPressed>();
-                std::cout << "Received key " << (keyPressed->scancode == sf::Keyboard::Scancode::X ? "X" : "(other)") << "\n";
-                if(keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
-                    shouldExit = true;
+
+            if (event->is<sf::Event::KeyPressed>()) {
+                if (event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::E) {
+                    player.switchWeaponNext();
+                }
+                if (event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Q) {
+                    player.switchWeaponPrev();
+                }
+                if (event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::R) {
+                    player.reload();
                 }
             }
         }
-        if(shouldExit) {
-            window.close();
-            std::cout << "Fereastra a fost închisă (shouldExit == true)\n";
-            break;
-        }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
 
-        window.clear();
+        sf::Vector2i mousePositionWindow = sf::Mouse::getPosition(window);
+        sf::Vector2f mousePositionWorld = window.mapPixelToCoords(mousePositionWindow,camera);
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && shootTimer.getElapsedTime().asSeconds() > player.getCurrentWeaponCooldown()
+            && player.canShoot(mousePositionWorld)) {
+            //Creez glnt si trag
+            bullets.push_back((player.shoot(mousePositionWorld)));
+            shootTimer.restart();
+        }
+
+
+        player.update(dt.asSeconds(), mousePositionWorld);
+        camera.setCenter(player.getPosition());
+
+        sf::Vector2f playerPos = player.getPosition();
+        for ([[maybe_unused]] auto& enemy : enemies) {
+            Enemy::update(dt.asSeconds(), playerPos);
+        }
+
+        for (auto& bullet : bullets) {
+            bullet.update(dt.asSeconds());
+        }
+
+        std::string ammoString = std::to_string(player.getCurrentAmmo())+ " / " + std::to_string(player.getReserveAmmo());
+        ammoText.setString(ammoString);
+
+        window.clear(sf::Color(30, 30, 30));
+        window.setView(camera);
+        gameMap.draw(window);
+        player.draw(window);
+
+        for (auto& enemy : enemies) {
+            enemy.draw(window);
+        }
+        for (auto& bullet : bullets) {
+            bullet.draw(window);
+        }
+        window.setView(window.getDefaultView());
+
+        sf::Vector2f viewSize = window.getDefaultView().getSize();
+        ammoText.setPosition({10.f, viewSize.y - static_cast<float>(ammoText.getCharacterSize()) - 10.f});
+
+        window.draw(ammoText);
+        player.drawUI(window);
+
         window.display();
     }
 
-    std::cout << "Programul a terminat execuția\n";
     return 0;
 }
